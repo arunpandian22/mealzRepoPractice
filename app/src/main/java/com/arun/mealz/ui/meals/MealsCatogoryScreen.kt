6f1336f5aab200e9434429d80747b67e8d023137
Arun Pandian <arunpandianm@Aruns-MacBook-Pro.local>
2022-07-25 22:58:44 +0530

    Implemented: expanding list items

--- app/src/main/java/com/arun/mealz/ui/meals/MealsCatogoryScreen.kt
+++ app/src/main/java/com/arun/mealz/ui/meals/MealsCatogoryScreen.kt
@@ -1,18 +1,25 @@
 package com.arun.mealz.ui.meals
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Bottom
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -21,7 +28,6 @@ import coil.request.ImageRequest
 import coil.transform.RoundedCornersTransformation
 import com.arun.mealz.model.MealsResponse
 import com.arun.mealz.ui.theme.MealzTheme
-import com.arun.mealz.ui.theme.Typography
 
 
 /**
@@ -43,6 +49,9 @@ fun MealsCategoriesScreen() {
 
 @Composable
 fun MealCategory(mealsResponse: MealsResponse) {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
     Card(
         shape = RoundedCornerShape(8.dp),
         elevation = 8.dp,
@@ -50,7 +59,7 @@ fun MealCategory(mealsResponse: MealsResponse) {
             .fillMaxWidth()
             .padding(top = 16.dp)
     ) {
-        Row() {
+        Row(modifier = Modifier.animateContentSize()) {
             val request =
                 ImageRequest.Builder(LocalContext.current).data(mealsResponse.imageUrl)
                     .transformations(
@@ -61,16 +70,39 @@ fun MealCategory(mealsResponse: MealsResponse) {
                 contentDescription = "",
                 modifier = Modifier
                     .size(88.dp)
-                    .padding(4.dp),
+                    .padding(4.dp)
+                    .align(alignment = Alignment.CenterVertically),
                 contentScale = ContentScale.Crop
             )
             Column(
                 modifier = Modifier
                     .align(alignment = Alignment.CenterVertically)
                     .padding(16.dp)
+                    .fillMaxWidth(0.8f)
             ) {
                 Text(text = mealsResponse.name, style = MaterialTheme.typography.h6)
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(
+                        text = mealsResponse.description,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = if (isExpanded) 10 else 4,
+                        style = MaterialTheme.typography.subtitle2,
+                        textAlign = TextAlign.Start
+                    )
+                }
             }
+            Icon(
+                imageVector = if (isExpanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Expand row icon",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(
+                        alignment = if (isExpanded) Bottom
+                        else CenterVertically
+                    )
+                    .clickable { isExpanded = !isExpanded }
+            )
+
         }
     }
 
